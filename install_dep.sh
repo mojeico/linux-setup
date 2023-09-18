5f1b0517b3155c19af22121cce907e299f9b5c4b
Gleb Mojeico <g.mojeico@gmail.com>
2023-09-18 12:44:29 -0400

    refactoring

--- install_dep.sh
+++ install_dep.sh
@@ -1,23 +1,59 @@
 echo "test111"
 
 
-####################################
-########## INSTALL UTILS ###########
-####################################
-sudo yum -y update
-sudo yum -y upgrade 
-sudo dnf -y install snapd
+#########################################
+########## UPGRADE && UPDATE  ###########
+#########################################
 
 
-sudo yum -y install epel-release
+sudo yum -y update && sudo yum -y upgrade 
+sudo yum -y install epel-release snapd
+
+
+############################
+########### TERMINAL ####### after this step - source ~/.zshrc
+############################
+
+
+sudo yum -y install neovim
+
+sudo dnf -y install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sudo yum -y install util-linux-user
+
+gh repo clone romkatv/powerlevel10k  $ZSH_CUSTOM/themes/powelevel10k/
+sed -i 's#robbyrussell#powerlevel10k/powerlevel10k#g' ~/.zshrc
+
+
+gh repo clone mojeico/nvim ~/.config/nvim
+
+
+chsh -s $(which zsh)
+
+sudo dnf copr enable atim/lazygit -y
+sudo dnf -y install lazygit
+
+sudo dnf -y install fzf
+sudo yum install -y tmux
+
+gh repo clone mojeico/tmux
+ln -s ~/tmux/.tmux .tmux
+ln -s ~/tmux/.tmux.conf .tmux.conf 
+
+source ~/.zshrc
+
+
+##########################
+####### TOOLS ############
+#########################
+
+
 sudo yum -y install htop
-
-sudo yum -y golang
-sudo yum -y nodejs
-
+sudo yum -y golang nodejs yum-utils
 sudo yum -y group install "Development Tools"
 
-sudo yum install -y yum-utils
+
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
 
@@ -35,10 +71,13 @@ sudo dnf -y install k9s
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.zshrc
 
+brew install kubectx
+
 
 #########################
 ######## UI Part ########
 #########################
+
 
 sudo dnf -y install gnome-tweak-tool
 
@@ -64,34 +103,7 @@ git config --global user.email "g.mojeico@gmail.com"
 
 
 ############################
-########### TERMINAL ####### after this step - source ~/.zshrc
+############ TEST ##########
 ############################
-
-sudo yum -y install neovim
-
-sudo dnf -y install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-sudo yum -y install util-linux-user
-
-gh repo clone romkatv/powerlevel10k  $ZSH_CUSTOM/themes/powelevel10k/
-sed -i 's#robbyrussell#powerlevel10k/powerlevel10k#g' ~/.zshrc
-
-
-
-gh repo clone mojeico/nvim ~/.config/nvim
-
-
-chsh -s $(which zsh)
-
-sudo dnf copr enable atim/lazygit -y
-sudo dnf -y install lazygit
-
-sudo dnf -y install fzf
-sudo yum install -y tmux
-
-gh repo clone mojeico/tmux
-ln -s ~/tmux/.tmux .tmux
-ln -s ~/tmux/.tmux.conf .tmux.conf 
 
 
